cbeff831cffb8eab654d3d0b1df149fbd2831d66
beny64 <31683377+beny64@users.noreply.github.com>
2018-12-18 14:58:54 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,114 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Editor, EditorState, convertFromRaw, convertToRaw, CompositeDecorator } from "draft-js";
+import {
+    registerCopySource,
+    handleDraftEditorPastedText,
+  } from "draftjs-conductor";
+
+import "./style.css";
+
+const initial = {
+    "blocks": [{
+        "depth": 0,
+        "key": "co9q8",
+        "text": "---before---",
+        "type": "unstyled"
+    }, {
+        "depth": 0,
+        "entityRanges": [{
+            "key": 0,
+            "length": 1,
+            "offset": 0
+        }],
+        "key": "3p8hv",
+        "text": " ",
+        "type": "atomic"
+    }, {
+        "depth": 0,
+        "key": "64jnl",
+        "text": "---after---",
+        "type": "unstyled"
+    }],
+    "entityMap": {
+        0: {
+            "data": {
+                "text": "This text comes form the entity data",
+            },
+            "mutability": "IMMUTABLE",
+            "type": "SNIPPET"
+        }
+    }
+};
+
+const Snippet = ({ entityKey, contentState }) => {
+    const { text } = contentState.getEntity(entityKey).getData();
+    return (<div className="snippet" contentEditable={false}>
+        Static text created by decorator
+        <div contentEditable={false}>{text}</div>
+    </div>);
+};
+
+const decorator = new CompositeDecorator([{
+    strategy: (contentBlock, callback, contentState) => {
+        contentBlock.findEntityRanges(
+            (character) => {
+                const entityKey = character.getEntity();
+                return (
+                    entityKey !== null &&
+                    contentState.getEntity(entityKey).getType() === 'SNIPPET'
+                );
+            },
+            callback
+        );
+    },
+    component: Snippet,
+}]);
+
+class MyEditor extends React.Component {
+    state = {
+        editorState: EditorState.createWithContent(convertFromRaw(initial), decorator),
+    }
+
+    componentDidMount() {
+        this.copySource = registerCopySource(this.editorRef);
+    }
+
+    componentWillUnmount() {
+        if (this.copySource) {
+            this.copySource.unregister();
+        }
+    }
+
+    handlePastedText = (text, html, editorState) => {
+        let newState = handleDraftEditorPastedText(html, editorState);
+
+        if (newState) {
+            console.log("draftjs-conductor handleDraftEditorPastedText applied")
+            this.onChange(newState);
+            return true;
+        }
+
+        return false;
+    };
+
+    onChange = (editorState) => {
+        console.log(convertToRaw(editorState.getCurrentContent()))
+        this.setState({ editorState });
+    };
+
+    render() {
+        return (<Editor
+            ref={(ref) => {
+                this.editorRef = ref;
+            }}
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            handlePastedText={this.handlePastedText}
+        />);
+    }
+}
+
+ReactDOM.render(<MyEditor />, document.getElementById("app"));
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
